12cb2dbc98ced18708bb1a7ec9e185f2e970fd09
JonasBrosers <105449805+JonasBrosers@users.noreply.github.com>
2022-10-10 17:20:49 -0300

    correção de pictureboxes e inicio da programacão dos eventos dos mesmos

--- FrontendDesktop/Form1.Designer.cs
+++ FrontendDesktop/Form1.Designer.cs
@@ -28,25 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pb_solo = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tm_solo = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_solo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pb_solo
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(548, 85);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(150, 64);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.pb_solo.Image = ((System.Drawing.Image)(resources.GetObject("pb_solo.Image")));
+            this.pb_solo.Location = new System.Drawing.Point(548, 85);
+            this.pb_solo.Name = "pb_solo";
+            this.pb_solo.Size = new System.Drawing.Size(150, 64);
+            this.pb_solo.TabIndex = 4;
+            this.pb_solo.TabStop = false;
+            this.pb_solo.Click += new System.EventHandler(this.tra);
+            this.pb_solo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb_solo_MouseClick);
+            this.pb_solo.MouseEnter += new System.EventHandler(this.pb_solo_MouseEnter);
+            this.pb_solo.MouseLeave += new System.EventHandler(this.pb_solo_MouseLeave);
             // 
             // pictureBox2
             // 
@@ -56,6 +62,7 @@
             this.pictureBox2.Size = new System.Drawing.Size(150, 64);
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
             // 
             // pictureBox3
             // 
@@ -75,19 +82,24 @@
             this.pictureBox4.TabIndex = 7;
             this.pictureBox4.TabStop = false;
             // 
+            // tm_solo
+            // 
+            this.tm_solo.Interval = 500;
+            this.tm_solo.Tick += new System.EventHandler(this.tm_solo_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1035, 450);
+            this.ClientSize = new System.Drawing.Size(925, 456);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pb_solo);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_solo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
@@ -96,9 +108,11 @@
         }
 
         #endregion
-        private PictureBox pictureBox1;
+        private PictureBox pb_solo;
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
         private PictureBox pictureBox4;
+        private System.Windows.Forms.Timer tm_solo;
+        private TrackBar trackBar1;
     }
 }
--- FrontendDesktop/Form1.cs
+++ FrontendDesktop/Form1.cs
@@ -6,8 +6,30 @@ namespace FrontendDesktop
         {
             InitializeComponent();
         }
+        private void pb_solo_MouseClick(object sender, MouseEventArgs e)
+        {
 
-        private void button1_Click(object sender, EventArgs e)
+        }
+
+
+
+        private void pb_solo_MouseEnter(object sender, EventArgs e)
+        {
+            tm_solo.Start();
+        }
+
+        private void pb_solo_MouseLeave(object sender, EventArgs e)
+        {
+            tm_solo.Stop();
+        }
+
+        private void tm_solo_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Foi garaio");
+            tm_solo.Stop();
+        }
+
+        private void tra(object sender, EventArgs e)
         {
 
         }
