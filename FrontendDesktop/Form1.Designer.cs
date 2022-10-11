e1749cf463b79a57ebc20453b370117a7097927b
Matheus Gomes <matheuzgomes82@gmail.com>
2022-10-11 16:59:13 -0300

    conclusao das picturesboxes

--- FrontendDesktop/Form1.Designer.cs
+++ FrontendDesktop/Form1.Designer.cs
@@ -32,13 +32,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pb_solo = new System.Windows.Forms.PictureBox();
             this.pb_duo = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pb_placar = new System.Windows.Forms.PictureBox();
+            this.pb_cores = new System.Windows.Forms.PictureBox();
             this.tm_solo = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pb_solo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_duo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_placar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_cores)).BeginInit();
             this.SuspendLayout();
             // 
             // pb_solo
@@ -49,7 +49,6 @@
             this.pb_solo.Size = new System.Drawing.Size(150, 73);
             this.pb_solo.TabIndex = 4;
             this.pb_solo.TabStop = false;
-            this.pb_solo.Click += new System.EventHandler(this.tra);
             this.pb_solo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb_solo_MouseClick);
             this.pb_solo.MouseEnter += new System.EventHandler(this.pb_solo_MouseEnter);
             this.pb_solo.MouseLeave += new System.EventHandler(this.pb_solo_MouseLeave);
@@ -59,30 +58,36 @@
             this.pb_duo.Image = ((System.Drawing.Image)(resources.GetObject("pb_duo.Image")));
             this.pb_duo.Location = new System.Drawing.Point(548, 178);
             this.pb_duo.Name = "pb_duo";
-            this.pb_duo.Size = new System.Drawing.Size(150, 64);
+            this.pb_duo.Size = new System.Drawing.Size(150, 73);
             this.pb_duo.TabIndex = 5;
             this.pb_duo.TabStop = false;
             this.pb_duo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb_duo_MouseClick);
             this.pb_duo.MouseEnter += new System.EventHandler(this.pb_duo_MouseEnter);
             this.pb_duo.MouseLeave += new System.EventHandler(this.pb_duo_MouseLeave);
             // 
-            // pictureBox3
+            // pb_placar
             // 
-            this.pictureBox3.Image = global::FrontendDesktop.Properties.Resources.placar_fix_2;
-            this.pictureBox3.Location = new System.Drawing.Point(548, 266);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(150, 64);
-            this.pictureBox3.TabIndex = 6;
-            this.pictureBox3.TabStop = false;
+            this.pb_placar.Image = global::FrontendDesktop.Properties.Resources.placar_fix_2;
+            this.pb_placar.Location = new System.Drawing.Point(548, 271);
+            this.pb_placar.Name = "pb_placar";
+            this.pb_placar.Size = new System.Drawing.Size(150, 73);
+            this.pb_placar.TabIndex = 6;
+            this.pb_placar.TabStop = false;
+            this.pb_placar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb_placar_MouseClick);
+            this.pb_placar.MouseEnter += new System.EventHandler(this.pb_placar_MouseEnter);
+            this.pb_placar.MouseLeave += new System.EventHandler(this.pb_placar_MouseLeave);
             // 
-            // pictureBox4
+            // pb_cores
             // 
-            this.pictureBox4.Image = global::FrontendDesktop.Properties.Resources.cores_fix_2;
-            this.pictureBox4.Location = new System.Drawing.Point(548, 349);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(150, 64);
-            this.pictureBox4.TabIndex = 7;
-            this.pictureBox4.TabStop = false;
+            this.pb_cores.Image = global::FrontendDesktop.Properties.Resources.cores_fix_2;
+            this.pb_cores.Location = new System.Drawing.Point(548, 363);
+            this.pb_cores.Name = "pb_cores";
+            this.pb_cores.Size = new System.Drawing.Size(150, 73);
+            this.pb_cores.TabIndex = 7;
+            this.pb_cores.TabStop = false;
+            this.pb_cores.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb_cores_MouseClick);
+            this.pb_cores.MouseEnter += new System.EventHandler(this.pb_cores_MouseEnter);
+            this.pb_cores.MouseLeave += new System.EventHandler(this.pb_cores_MouseLeave);
             // 
             // tm_solo
             // 
@@ -94,17 +99,17 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(925, 456);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.pb_cores);
+            this.Controls.Add(this.pb_placar);
             this.Controls.Add(this.pb_duo);
             this.Controls.Add(this.pb_solo);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pb_solo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_duo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_placar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_cores)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -112,8 +117,8 @@
         #endregion
         private PictureBox pb_solo;
         private PictureBox pb_duo;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox4;
+        private PictureBox pb_placar;
+        private PictureBox pb_cores;
         private System.Windows.Forms.Timer tm_solo;
         private TrackBar trackBar1;
     }
--- FrontendDesktop/Form1.cs
+++ FrontendDesktop/Form1.cs
@@ -6,13 +6,12 @@ namespace FrontendDesktop
         {
             InitializeComponent();
         }
+
+        // picture box solo
         private void pb_solo_MouseClick(object sender, MouseEventArgs e)
         {
             pb_solo.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\solopress2.jpg");
         }
-
-
-
         private void pb_solo_MouseEnter(object sender, EventArgs e)
         {
             pb_solo.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\soloup2.jpg");
@@ -28,29 +27,53 @@ namespace FrontendDesktop
 
         }
 
-        private void tra(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pb_duo_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        // picture box duo
         private void pb_duo_MouseClick(object sender, MouseEventArgs e)
         {
-
+            pb_duo.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\duopress.jpg");
         }
 
         private void pb_duo_MouseEnter(object sender, EventArgs e)
         {
-
+            pb_duo.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\duoup.jpg");
         }
 
         private void pb_duo_MouseLeave(object sender, EventArgs e)
         {
-            pb_solo.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\duofix.jpg");
+            pb_duo.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\duofix.jpg");
+        }
+
+        // picture box placar
+        private void pb_placar_MouseClick(object sender, MouseEventArgs e)
+        {
+            pb_placar.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\placarpress.jpg");
+
+        }
+
+        private void pb_placar_MouseEnter(object sender, EventArgs e)
+        {
+            pb_placar.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\placarup.jpg");
+        }
+
+        private void pb_placar_MouseLeave(object sender, EventArgs e)
+        {
+            pb_placar.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\placarfix.jpg");
+        }
+
+        //picture box cores
+        private void pb_cores_MouseClick(object sender, MouseEventArgs e)
+        {
+            pb_cores.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\corespress.jpg");
+        }
+
+        private void pb_cores_MouseEnter(object sender, EventArgs e)
+        {
+            pb_cores.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\coresup.jpg");
+        }
+
+        private void pb_cores_MouseLeave(object sender, EventArgs e)
+        {
+            pb_cores.Image = Image.FromFile(@"E:\ProjetoPongII\FrontendDesktop\img\coresfix.jpg");
         }
     }
 }
